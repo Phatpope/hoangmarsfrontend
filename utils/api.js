1789006128214ad8f7e2a3dbec931fea7b5b61d3
Phatpope <ikavo799@gmail.com>
2024-03-24 20:28:10 +0700

    ignore build eslink rule

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,9 @@
 
 
 const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-const API_URL1 = "http://127.0.0.1:1337"
+// const API_URL1 = "http://127.0.0.1:1337"
+const API_URL1 = process.env.NEXT_PUBLIC_API_URL
+
 
 export const fetchDataFromApi = async (endpoint) => {
     const options = {
