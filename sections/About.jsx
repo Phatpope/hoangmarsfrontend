3cf6a9360c0c997ad22575692adb8523597510ea
Phatpope <ikavo799@gmail.com>
2024-03-27 17:35:00 +0700

    save meilisearch

--- sections/About.jsx
+++ sections/About.jsx
@@ -22,17 +22,11 @@ const About = () => (
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-white">MarApple</span> ShopDunk Care là trung tâm bảo hành ủy quyền chính thức của Apple, 
-         với những kỹ thuật viên chuyên nghiệp và tận tâm, được Apple đào tạo. ShopDunk Care tuân theo những tiêu chuẩn nghiêm ngặt và cao cấp, cam kết
-         sử dụng những linh kiện chính thức từ Apple nhằm mang lại trải nghiệm Apple tốt nhất cho quý khách hàng khi mua các sản phẩm tại ShopDunk.{' '}
-        <span className="font-extrabold text-white">
-          MarApple
-        </span>{' '}
-        Chuyen nghiep, using only{' '}
-        <span className="font-extrabold text-white">VR</span> Máy được thay thế theo đúng tiêu chuẩn và chính sách của Apple.
+        <span className="font-extrabold text-white">MarApple</span> là chuyên cung cấp sản phẩm Apple, 
+         với những kỹ thuật viên chuyên nghiệp và tận tâm, được Apple đào tạo. Shop luôn tuân theo những tiêu chuẩn nghiêm ngặt và cao cấp, cam kết
+         sử dụng những linh kiện chính thức từ Apple nhằm mang lại trải nghiệm Apple tốt nhất cho quý khách hàng khi mua các sản phẩm tại đây.{' '}
          Thời gian bảo hành hãng lên đến 90 ngày hoặc theo thời gian bảo hành còn lại của thiết bị quý khách.. Let's{' '}
-        <span className="font-extrabold text-white">explore</span> MarApple
-        de nhan nhieu uu dai.
+      
       </motion.p>
 
       <motion.img
